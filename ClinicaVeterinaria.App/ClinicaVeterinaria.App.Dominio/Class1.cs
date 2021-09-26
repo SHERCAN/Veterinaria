a5a5d91f1d8919581619d423c56134790dd99556
SHERCAN <sidereoyess@gmail.com>
2021-09-26 13:38:27 -0500

    version2

--- ClinicaVeterinaria.App/ClinicaVeterinaria.App.Dominio/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ClinicaVeterinaria.App.Dominio
-{
-    public class Class1
-    {
-    }
-}
--- ClinicaVeterinaria.App/ClinicaVeterinaria.App.Persistencia/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ClinicaVeterinaria.App.Persistencia
-{
-    public class Class1
-    {
-    }
-}
